1e2fbefa272742d2b6e78941645c0f267a1bbc0a
ayitech <charles@ayitech.com>
2020-03-11 08:51:13 -0500

    Update WebApiConfig.cs

--- BasicWebApi/App_Start/WebApiConfig.cs
+++ BasicWebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace BasicWebApi
@@ -9,6 +11,9 @@ namespace BasicWebApi
     {
         public static void Register(HttpConfiguration config)
         {
+            //Comment this if you want to return xml
+            config.Formatters.Add(new CustomJsonFormatter());
+
             // Web API configuration and services
 
             // Web API routes
@@ -19,6 +24,20 @@ namespace BasicWebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        //json data return
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        {
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
         }
     }
 }
